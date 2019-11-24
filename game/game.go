c14bc7519bad7417d4d08f9939f4abce0efb14e5
Pavlo Goncharenko <pashaghnp@gmail.com>
2019-11-24 23:47:28 +0200

    Create new game.

--- game/game.go
+++ game/game.go
@@ -1,20 +1,22 @@
 package game
 
-import "alien-invasion/alien"
+import (
+	"alien-invasion/alien"
+	"io"
+)
 
 type Game struct {
 	alien.Aliens
-	Map
+	*Map
 }
 
-func NewGame(aliensNumber int) *Game {
+func NewGame(mapReader io.Reader, aliensNumber int) *Game {
 	//create aliens
 	aliens := alien.CreateAliens(aliensNumber)
 
-	game := &Game{Aliens: aliens}
+	//create Map
+	m := CreateMap(mapReader)
+
+	game := &Game{Aliens: aliens, Map: m}
 	return game
-}
-
-func (game *Game) CreateAliens() {
-
 }
--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"alien-invasion/game"
+	"flag"
+	"log"
+	"os"
+)
 
+func main() {
+	//read parameters from the command line
+	inputFileFlag := flag.String("input", "map.txt", "txt file which contains information about world map")
+	aliensNumberFlag := flag.Int("aliens", 10, "Number of aliens")
+	flag.Parse()
+
+	//open file with input data
+	mapFile, err := os.Open(*inputFileFlag)
+	if err != nil {
+		log.Fatalln("error while opening input CSV file: ", err)
+		return
+	}
+	defer closeFile(mapFile)
+
+	//create new game
+	g := game.NewGame(mapFile, *aliensNumberFlag)
+}
+
+// Close file and logs error if any
+func closeFile(file *os.File) {
+	if err := file.Close(); err != nil {
+		log.Fatalf("error while closing file %v: %v", file.Name(), err)
+	}
 }
